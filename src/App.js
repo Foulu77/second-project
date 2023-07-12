9950ab53b9ad655e6a59079111660a18dd718788
Foulu <129785761+Foulu77@users.noreply.github.com>
2023-07-12 14:29:05 -0600

    updated code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Steps />
     </div>
   );
 }
 
-export default App;
+function Steps() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+  const myDate = new Date();
+  const formattedDAte = new Date(
+    myDate.setDate(myDate.getDate() + count)
+  ).toDateString();
+
+  function deductSteps() {
+    if (step > 1) setStep((stepper) => stepper - 1);
+  }
+
+  function sumSteps() {
+    setStep((stepper) => stepper + 1);
+  }
+
+  function countMinus() {
+    setCount((counter) => counter - 1 * step);
+  }
+
+  function countPlus() {
+    setCount((counter) => counter + 1 * step);
+  }
+
+  return (
+    <>
+      <div className="container">
+        <div className="group-1">
+          <button className="item" onClick={deductSteps}>
+            -
+          </button>
+          <p className="item">Steps: {step}</p>
+          <button className="item" onClick={sumSteps}>
+            +
+          </button>
+        </div>
+        <div className="group-2">
+          <button className="item" onClick={countMinus}>
+            -
+          </button>
+          <p className="item">count: {count}</p>
+          <button className="item" onClick={countPlus}>
+            +
+          </button>
+        </div>
+        <div className="group-3">
+          {count === 0 && <p className="item">{`Today is ${formattedDAte}`}</p>}
+          {count > 0 && (
+            <p className="item">{`${count} days from today is ${formattedDAte}`}</p>
+          )}
+          {count < 0 && (
+            <p className="item">{`${count} days ago was ${formattedDAte}`}</p>
+          )}
+        </div>
+      </div>
+    </>
+  );
+}
